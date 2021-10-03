158da7f235427d6fb924bc2cd40d4e8142c7a657
lasnotasdigitales <89501557+lasnotasdigitales@users.noreply.github.com>
2021-10-03 11:42:41 -0500

    Update config.h

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:pixelsize=16:antialias=true:autohint=true";
+static char *font = "mononoki-Regular:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
